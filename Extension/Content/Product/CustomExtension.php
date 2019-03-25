ee16a4318758421480d7fa8fcdb1c6400a88c2bd
Patrick Stahl <p.stahl@shopware.com>
2019-03-25 07:42:07 +0100

    Split extension into subscriber + extension

--- Extension/Content/Product/CustomExtension.php
+++ Extension/Content/Product/CustomExtension.php
@@ -2,26 +2,14 @@
 
 namespace EntityExtension\Extension\Content\Product;
 
-use EntityExtension\Struct\MyCustomStruct;
 use Shopware\Core\Content\Product\ProductDefinition;
-use Shopware\Core\Content\Product\ProductEntity;
-use Shopware\Core\Content\Product\ProductEvents;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityExtensionInterface;
-use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityLoadedEvent;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Deferred;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\ObjectField;
 use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class CustomExtension implements EntityExtensionInterface, EventSubscriberInterface
+class CustomExtension implements EntityExtensionInterface
 {
-    public static function getSubscribedEvents(): array
-    {
-        return [
-            ProductEvents::PRODUCT_LOADED_EVENT => 'onProductsLoaded'
-        ];
-    }
-
     public function extendFields(FieldCollection $collection): void
     {
         $collection->add(
@@ -32,13 +20,5 @@ class CustomExtension implements EntityExtensionInterface, EventSubscriberInterf
     public function getDefinitionClass(): string
     {
         return ProductDefinition::class;
-    }
-
-    public function onProductsLoaded(EntityLoadedEvent $event)
-    {
-        /** @var ProductEntity $productEntity */
-        foreach ($event->getEntities() as $productEntity) {
-            $productEntity->addExtension('custom_struct', new MyCustomStruct());
-        }
     }
 }
---
+++ Subscriber/MySubscriber.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace EntityExtension\Subscriber;
+
+use EntityExtension\Struct\MyCustomStruct;
+use Shopware\Core\Content\Product\ProductEntity;
+use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityLoadedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Shopware\Core\Content\Product\ProductEvents;
+
+class MySubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        // Return the events to listen to as array like this:  <event to listen to> => <method to execute>
+        return [
+            ProductEvents::PRODUCT_LOADED_EVENT => 'onProductsLoaded'
+        ];
+    }
+
+    public function onProductsLoaded(EntityLoadedEvent $event): void
+    {
+        /** @var ProductEntity $productEntity */
+        foreach ($event->getEntities() as $productEntity) {
+            $productEntity->addExtension('custom_struct', new MyCustomStruct());
+        }
+    }
+}
